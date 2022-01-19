a0faa3ede8f81a985be79ef8af5c8291c944ffb5
athu16 <atharva16vaidya@gmail.com>
2022-01-19 14:27:52 +0530

    updated database interfacing code

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ database = sqlite3.connect("college_rooms.db")
 # Create a cursor object, used to make changes to the database
 cur = database.cursor()
 
-# A function to create a table, pass if it already exists
+# A function to create a table, skip if it already exists
 def CreateTable(command):
     try:
         global cur
@@ -17,6 +17,7 @@ def CreateTable(command):
         pass
 
 # A function to check if a room is available
+# Returns 0 if the room is not available, 1 if it is available
 def CheckAvailability(roomId):
     try:
         global cur
@@ -28,7 +29,8 @@ def CheckAvailability(roomId):
         # print(err)
         print("The requested room does not exist in the database.")
 
-# A function to add a new entry to both the tables
+# A function to add a new room to the database
+# Requires the location and the type of room
 def AddEntry(Location, Type):
     global cur
     command = "INSERT INTO rooms VALUES (" + Location + ", 1, " + Type + ")"
@@ -36,6 +38,7 @@ def AddEntry(Location, Type):
     cur.execute(command)
 
 # Function to initialize the database with values for all classrooms
+# Requires the number of buildings, floors and rooms
 def InitDatabase(Buildings, Floors, Rooms):
     try:
         global cur
@@ -50,33 +53,58 @@ def InitDatabase(Buildings, Floors, Rooms):
         pass
                 
 # A function which allows the user to book a particular room at a certain date / time for a particular duration
+# Returns the location of the chosen room if successful, else if all rooms of the required type are booked, returns -1
 def BookRoom(Building, RoomType, StartTime, Duration):
     try:
         global cur
         
-        # Select a random available room of the requested type
-        roomSelectionCommand = "SELECT Location from rooms WHERE Type = " + RoomType + " AND Availability = 1"
+        # Select a random available room of the requested type in a particular building
+        roomSelectionCommand = "SELECT Location from rooms WHERE Type = '" + RoomType + "' AND Availability = 1 AND Location LIKE 'B" + str(Building) + "%'"
         availableRooms = cur.execute(roomSelectionCommand).fetchall()
         assignedRoom = random.choice(availableRooms) [0]
 
         # Execute the commands to insert booking information into both the tables
-        command = "INSERT INTO timetable VALUES ('" + assignedRoom + "', " + StartTime + ", " + Duration + ")"
+        command = "INSERT INTO timetable VALUES ('" + assignedRoom + "', '" + StartTime + "', '" + Duration + "')"
         notavailable = "UPDATE rooms SET Availability = 0 WHERE Location = '" + assignedRoom + "'"
         cur.execute(command)
         cur.execute(notavailable)
         return assignedRoom
     except Exception as err:
-        print("No more rooms of the seleccted type are are available!")
+        print("No more rooms of the selected type in the selected building are are available!")
         return -1
 
+# A function to return all available rooms of a particular type
+# Returns available rooms as a list, -1 if there are no available rooms
+def AvailableRooms(RoomType):
+    try:
+        global cur
+        roomSelectionCommand = "SELECT Location from rooms WHERE Type = '" + RoomType + "' AND Availability = 1"
+        availableRoomsString = cur.execute(roomSelectionCommand).fetchall()
+        availableRooms = []
+        for i in range(0, len(availableRoomsString)):
+            availableRooms.append (availableRoomsString[i][0])
+        
+        # Check if the list is empty, i.e. no available rooms
+        if len(availableRooms) == 0:
+            return -1
+        else:
+            return availableRooms
+    except:
+        pass
 
-# Create the table which will store information about rooms.
+# Create the table which will store information about rooms
 # This table contains the room's location, its availability status and the type (i.e. capacity)
 CreateTable("CREATE TABLE rooms (Location varchar(10) PRIMARY KEY, Availability int, Type varchar(20))")
+
+# Create the table which will store the list of booked rooms and their timings
 CreateTable("CREATE TABLE timetable (Location varchar(10) PRIMARY KEY, Initiation_Time smalldatetime, Duration time, FOREIGN KEY (Location) REFERENCES rooms(Location))")
 
-InitDatabase(6, 5, 10)
+# Initialzing the database with all available classrooms in the campus
+# The first argument is the number of buildings, the second is the number of floors on each building and third is the number of rooms on each floor
+# The type of each room is assigned randomly, but would be filled out manually in a real-life scenario
+InitDatabase(4, 3, 4)
 
-print(BookRoom("1", "'Classroom'", "'2022-01-14 12:00:00'", "'01:00:00'"))
+print(BookRoom("2", "Classroom", "2022-01-14 12:00:00", "01:00:00"))
+print(AvailableRooms("Classroom"))
 
 database.commit()
